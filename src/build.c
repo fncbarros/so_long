e9f4331b0c901a669ffcfeafdd131a3cec226062
fbarros <fbarros@student.42lisboa.com>
2021-08-16 18:03:17 +0100

    Restructure

--- src/build.c
+++ src/build.c
@@ -9,6 +9,9 @@
 /*   Updated: 2021/08/16 17:19:31 by fbarros          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../inc/so_long.h"
+
 /*
             build_P ??
             build_C ??  or build_PCE
@@ -16,3 +19,14 @@
 
             build_background or build_map along with walls and junk
 */
+
+void	paint_window(t_data *img, const int w, const int h)
+/*Painting the whole window to cover asset background
+4743554*/
+{
+	int	z;
+
+    z = -1;
+	while (++z < (w * h))
+		img->addr[z] = 4743554;
+}
--- src/main.c
+++ src/main.c
@@ -12,7 +12,25 @@
 
 #include "../inc/so_long.h"
 
+/*Need structs or flags in order for it to be easier to free all memory in case of error*/
 
+/*PSEUDO (or blueprint rather):
+	read map
+	check if it's valid
+		valid chars 01CEP(only 1 P and 1 E)
+		1st and last line needs be all 1's and all others need start and end w/ a 1
+		total needs to be equal to no of lines * lenght of 1st line
+		if not display err ("Error\n" followed by an explicit error message)
+	mlx init
+	build images
+	build window (wall image line lenght (in pixels) * map width (in 1's))
+		(paint window...)
+	build map through array (1d/2d ??) (send every image to window)
+	build keyhooks (WASD, ESC, red cross button, need free all memory in such cases)
+		Actions: movement, collecting and exiting
+		Requires printing images for each action
+	
+	*/
 
 int	main(int argc, char **argv)
 /*W_WIDTH/W_HEIGHT dependent on map and asset sizes.......*/
@@ -47,8 +65,11 @@ int	main(int argc, char **argv)
 	g.character.img.h = W_HEIGHT;
 	g.character.img.p = mlx_xpm_file_to_image(mlx, "img/marvin.xpm", &g.character.img.w, &g.character.img.h);
 	g.character.addr = (int *)mlx_get_data_addr(g.character.img.p, &g.character.bpp, &g.character.line, &g.character.endian); /*getting data from image but also mallocs(??)*/	
+	g.collect.img.p = mlx_xpm_file_to_image(mlx, "img/walls.xpm", &g.collect.img.w, &g.collect.img.h);
+	g.collect.addr = (int *)mlx_get_data_addr(g.collect.img.p, &g.collect.bpp, &g.collect.line, &g.collect.endian); /*getting data from image but also mallocs(??)*/	
 
 	mlx_put_image_to_window(mlx, win.p, g.character.img.p, 600, 400);
+	mlx_put_image_to_window(mlx, win.p, g.collect.img.p, 1200, 800);
 	mlx_loop(mlx);
 
 	mlx_destroy_image(mlx, g.background.img.p); /*free memory ??*/
