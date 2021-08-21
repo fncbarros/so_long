b3f194911a99ca2e5ea1e747ebe536c955b90da7
fbarros <fbarros@student.42lisboa.com>
2021-08-21 19:05:14 +0100

    managed to read it all but segfaulted

--- src/map.c
+++ src/map.c
@@ -12,6 +12,26 @@
 
 #include "../inc/so_long.h"
 
+static char	**two_dimension_realloc(char ***p_arr, int size)
+{
+	char	**tmp;
+	char	**tmp2;
+	int		i;
+
+	i = -1;
+	// tmp = malloc(sizeof(char **) * (size + 1);
+	tmp2 = *p_arr;
+	tmp = ft_calloc(size + 1, sizeof(char **));
+	tmp[size] = NULL;
+	if (!tmp)
+		display_err(clear_map(*p_arr));
+	while (tmp2[++i] && i <= size)
+		tmp[i] = tmp2[i]/**p_arr[i]*/;
+	// free(*p_arr);
+	free(tmp2);
+	return (tmp);
+}
+
 int	read_map(char *map, t_map *m)
 /*-------------------------->LEAKING! REMAKE IT!! <-----------------------------
 Stores map in char *map, no of elements in the x axis and y axis in x and y (passed by reference)
@@ -26,7 +46,8 @@ Needs error handling and adding \n maybe...but maybe not
 	fd = open(map, O_RDONLY);
 	if (fd < 0)
 		display_err(0); //or perror(strerror(errno)); exit(1);
-	m->addr = malloc_n_check(sizeof(char **), 0, 0);
+	m->addr = malloc_n_check(sizeof(char **) * 2, 0, 0);
+	// m->addr[1] = 0;
 	while (r > 0)
 	{
 		if (++m->rows == 1)
@@ -36,11 +57,12 @@ Needs error handling and adding \n maybe...but maybe not
 			r = -1;
 		else
 		/*Need buffer to */
-			m->addr = (char **)ft_realloc(m->addr, (m->rows + 1) * sizeof(char *)); //not gonna work
+			// m->addr = (char **)ft_realloc(m->addr, (m->rows + 1) * sizeof(char *)); //not gonna work
+			m->addr = two_dimension_realloc(&m->addr, m->rows + 1);
 		if (!m->addr)
 			r = -1;
 	}
-	*(m->addr + m->rows) = 0;
+	// *(m->addr + m->rows) = 0;
 	if (r == -1)
 		display_err(clear_map(m->addr));
 	close(fd);
@@ -64,13 +86,13 @@ void	build_map(char *m_path, t_elements *g)
 }
 
 
-// int	main(int argc, char **argv)
-// {
-// 	t_elements	g;
+int	main(int argc, char **argv)
+{
+	t_elements	g;
 
 
-// 	if (argc < 2)
-// 		display_err(2); //DISPLAY ERROR <----------
+	if (argc < 2)
+		display_err(2); //DISPLAY ERROR <----------
 
 
 	// /*----------------TEST-------------------*/
@@ -91,16 +113,16 @@ void	build_map(char *m_path, t_elements *g)
 	// /*----------------TEST-------------------*/
 
 
-// 	img_init(&g);
-// 	printf("%d\n", read_map(argv[1], &g.map));
-// 	int i = -1;
-// 	int total = 0;
-// 	while (g.map.addr[++i])
-// 	{
-// 		printf("%s\n", g.map.addr[i]);
-// 		total += ft_strlen(g.map.addr[i]);
-// 	}
-// 	printf("%d x %d == %d", g.map.columns, g.map.rows, total);
-// 	clear_map(g.map.addr);
-// }
+	img_init(&g);
+	printf("%d\n", read_map(argv[1], &g.map));
+	int i = -1;
+	int total = 0;
+	while (g.map.addr[++i])
+	{
+		printf("%s\n", g.map.addr[i]);
+		total += ft_strlen(g.map.addr[i]);
+	}
+	printf("%d x %d == %d", g.map.columns, g.map.rows, total);
+	clear_map(g.map.addr);
+}
 
